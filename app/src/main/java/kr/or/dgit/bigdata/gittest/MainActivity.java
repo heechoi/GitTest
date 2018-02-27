6ea21eccd62a5300a7b0648621b377e047a9b199
heechoi <ekgml9904@naver.com>
2018-02-27 16:03:44 +0900

    코드 수정했다 이놈아

--- app/src/main/java/kr/or/dgit/bigdata/gittest/MainActivity.java
+++ app/src/main/java/kr/or/dgit/bigdata/gittest/MainActivity.java
@@ -2,6 +2,8 @@ package kr.or.dgit.bigdata.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtn01(View view) {
+        Toast.makeText(getApplicationContext(),"버튼을 눌렀습니다~~~~!!",Toast.LENGTH_SHORT).show();
     }
 }
